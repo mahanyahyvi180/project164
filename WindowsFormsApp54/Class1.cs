33f6dfe2894f0cd934ae5d4a4f49071873fee33f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-02 21:04:23 +0330

    Add files via upload

---
+++ WindowsFormsApp54/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp54
+{
+    namespace teacher
+    { 
+        public class cEmployee
+        {
+            public string firstname;
+            public string lastname;
+            public string identity;
+
+            public cEmployee()
+            {
+                firstname = "ali";
+                lastname = "yahyavi";
+            }
+
+            public string Concatinate()
+            {
+                return firstname + ' ' + lastname;
+            }
+ 
+          
+           
+            }
+    }
+}
